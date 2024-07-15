86ec1b501a81cfac04c639d15ba5fe146eb46f55
srabhiram <sriramoji.abhi@gmail.com>
2024-07-15 13:16:06 +0530

    fix

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -12,7 +12,7 @@ export async function GET(req: NextRequest) {
       secure: process.env.NODE_ENV === 'production', // Expire the cookie immediately.
     });
 
-    return res.redirected;
+    return res;
   } catch (error: any) {
     return NextResponse.json(
       {
