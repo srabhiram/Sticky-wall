d7d665f245ab17c514fd8de917a7f4d8c71ca665
srabhiram <sriramoji.abhi@gmail.com>
2024-07-15 12:44:27 +0530

    v1.1.2 fix

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -8,7 +8,8 @@ export async function GET(req: NextRequest) {
     });
     res.cookies.set("token", "", {
       httpOnly: true,
-      expires: new Date(0), // Expire the cookie immediately.
+      expires: new Date(0),
+      secure: process.env.NODE_ENV === 'production', // Expire the cookie immediately.
     });
 
     return res;
