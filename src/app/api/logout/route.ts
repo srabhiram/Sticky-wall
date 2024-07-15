38a32b4570261048b29b8ba85fe496f5ead65474
srabhiram <sriramoji.abhi@gmail.com>
2024-07-15 12:31:55 +0530

    update

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,23 +1,26 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  
   try {
     const res = NextResponse.json({
       message: "Logout Successful",
       success: true,
     });
-    // Clear the token cookie by setting it with an expired date
-    res.cookies.delete("token")// Expire the cookie // Set the sameSite attribute for better security
-    ;
+    res.cookies.set("token", "", {
+      httpOnly: true,
+      expires: new Date(0), // Expire the cookie immediately.
+    });
 
     return res;
   } catch (error: any) {
-    return NextResponse.json({
-      message: error.message,
-      success: false,
-    }, {
-      status: 500
-    });
+    return NextResponse.json(
+      {
+        message: error.message,
+        success: false,
+      },
+      {
+        status: 500,
+      }
+    );
   }
 }
