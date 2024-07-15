f599b62e0949a059117908cfcfe34520d27dc2eb
srabhiram <sriramoji.abhi@gmail.com>
2024-07-15 12:27:26 +0530

    v1.1.2 logout api and cookies fixed

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,19 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
+  
   try {
     const res = NextResponse.json({
       message: "Logout Successful",
       success: true,
     });
-
     // Clear the token cookie by setting it with an expired date
-    res.cookies.set("token", "", {
-      httpOnly: true,
-      expires: new Date(0), // Expire the cookie
-      path: "/", // Ensure the path matches where the cookie was set
-      sameSite: "lax", // Set the sameSite attribute for better security
-    });
+    res.cookies.delete("token")// Expire the cookie // Set the sameSite attribute for better security
+    ;
 
     return res;
   } catch (error: any) {
