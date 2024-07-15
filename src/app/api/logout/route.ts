1a68d073c441d38aa926474cd688b9411cce6961
srabhiram <sriramoji.abhi@gmail.com>
2024-07-15 13:34:28 +0530

    fix

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,16 +1,19 @@
+import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
+  const cookie = cookies();
+  cookie.set('token', '',{
+    httpOnly: true,
+    expires: new Date(0),
+    secure: process.env.NODE_ENV === 'production', // Expire the cookie immediately.
+  })
   try {
     const res = NextResponse.json({
       message: "Logout Successful",
       success: true,
     });
-  res.cookies.set('token', '', {
-      httpOnly: true,
-      expires: new Date(0),
-      secure: process.env.NODE_ENV === 'production', // Expire the cookie immediately.
-    });
+  
 
     return res;
   } catch (error: any) {
