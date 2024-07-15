11217bb152effba30e97f82da2f5bcaaed854efa
srabhiram <sriramoji.abhi@gmail.com>
2024-07-15 11:47:53 +0530

    logout api fix

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,15 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
+
 export async function GET(req: NextRequest) {
   try {
     const res = NextResponse.json({
-      message: "Logout Succefull",
+      message: "Logout Successful",
       success: true,
     });
+
     res.cookies.set("token", "", {
       httpOnly: true,
+      expires: new Date(0), // Set expiration date to clear the cookie
     });
+
     return res;
   } catch (error: any) {
-    throw new Error (error.message)
+    return NextResponse.json({
+      message: error.message,
+      success: false,
+    }, {
+      status: 500
+    });
   }
 }
